622599ebe7493b33efd88e6f38900af28d246dd7
muhIkhsanResyah <bojeszpro@gmail.com>
2025-12-11 21:35:10 +0800

    Update 5. dictionary.py

--- final test/5. dictionary.py
+++ final test/5. dictionary.py
@@ -1,1 +1,23 @@
+# Contoh dictionary dasar
+
+person = {
+    "nama": "Budi",
+    "umur": 20,
+    "kota": "Jakarta"
+}
+
+print("Nama:", person["nama"])
+print("Daftar key:", list(person.keys()))
+print("Daftar value:", list(person.values()))
+
+# Menambah data
+person["pekerjaan"] = "Mahasiswa"
+
+# Mengubah data
+person["umur"] = 21
+
+# Menghapus data
+del person["kota"]
+
+print("Data setelah update:", person)
 
